d93af5b38f51608a14c13708867ddd09e83e38f7
Vidocq17 <AvidanB1704@gmail.com>
2023-04-20 17:21:43 +0200

    Commit Header page d'accueil

---
+++ src/Header.js
@@ -0,0 +1,25 @@
+import React from "react";
+function Header() {
+    return (
+      <div className="header">
+        <div className="logo">
+          <img src="https://www.logo.wine/a/logo/Netflix/Netflix-Logo.wine.svg" alt="Netflix logo" />
+        </div>
+        <div className="tabs">
+          <ul>
+            <li><a href="#">Accueil</a></li>
+            <li><a href="#">Séries</a></li>
+            <li><a href="#">Films</a></li>
+            <li><a href="#">Nouveautés les plus regardées</a></li>
+            <li><a href="#">Ma liste</a></li>
+            <li><a href="#">Explorer par langue</a></li>
+          </ul>
+        </div>
+        <div className="profile">
+          <img src="https://www.example.com/profile.jpg" alt="Profil" />
+        </div>
+      </div>
+    );
+  }
+  
+  export default Header;
