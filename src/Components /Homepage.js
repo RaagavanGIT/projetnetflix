834f0c3a19652899977d31aec1270ac17fe92ec5
Raagavan <r_sathyalingam@hetic.eu>
2023-04-21 15:39:04 +0200

    random movie refresh

--- src/Components /Homepage.js
+++ src/Components /Homepage.js
@@ -3,6 +3,7 @@ import './Homepage.css';
 
 function Homepage() {
   const [movies, setMovies] = useState([]);
+  const [randomMovie, setRandomMovie] = useState(null);
 
   useEffect(() => {
     const apiKey = '9e1d34262c59d085124aa1ee3b7065cb';
@@ -10,28 +11,40 @@ function Homepage() {
 
     fetch(apiUrl)
       .then((response) => response.json())
-      .then((data) => setMovies(data.results));
+      .then((data) => {
+        setMovies(data.results);
+        const randomIndex = Math.floor(Math.random() * data.results.length);
+        setRandomMovie(data.results[randomIndex]);
+      });
   }, []);
+
+  const handleRefresh = () => {
+    const randomIndex = Math.floor(Math.random() * movies.length);
+    setRandomMovie(movies[randomIndex]);
+  };
 
   return (
     <div className="homepage-container">
       <div className="homepage-banner_movie">
-        {movies.length > 0 && (
-            <img
-              src={'https://image.tmdb.org/t/p/original' + movies[0].backdrop_path}
-              alt={movies[0].title}
-            />
+        {randomMovie && (
+          <img
+            src={'https://image.tmdb.org/t/p/original' + randomMovie.backdrop_path}
+            alt={randomMovie.title}
+          />
         )}
         <button className='play'>Visionner</button>
         <button className='add_playlist'>Ajouter à ma liste</button>
+        <button onClick={handleRefresh}>Rafraîchir</button>
       </div>
       <div className="movies-grid">
         {movies.map((movie) => (
           <div key={movie.id} className="movie">
-            <a href="#" target="_blank"><img
-              src={'https://image.tmdb.org/t/p/w500' + movie.poster_path}
-              alt={movie.title}
-            /></a>
+            <a href="#" target="_blank">
+              <img
+                src={'https://image.tmdb.org/t/p/w500' + movie.poster_path}
+                alt={movie.title}
+              />
+            </a>
             <h2>{movie.title}</h2>
             <p>{movie.overview}</p>
           </div>
