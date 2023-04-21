7955628d8c7abc614c1dd91ba0c792d305212bbd
Raagavan <r_sathyalingam@hetic.eu>
2023-04-21 14:03:45 +0200

    on s'attaque au css du MoviePage

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import Homepage from '/Users/raagavan/Desktop/projetnetflix/src/Components /Homepage.js';
-import Header from '/Users/raagavan/Desktop/projetnetflix/src/Components /Header.js'; 
-// import { BrowserRouter, Route } from 'react-router-dom';
+import Header from '/Users/raagavan/Desktop/projetnetflix/src/Components /Header.js';
+import {Routes, Route } from 'react-router-dom';
 
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Homepage />
+      <Routes>
+        <Route path='/' element={<><Header/><Homepage/></>} />
+      </Routes>
     </div>
   );
 }
