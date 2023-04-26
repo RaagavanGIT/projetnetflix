b88714c7510a70d3d5e20fe85b24614fdaf5f926
Vidocq17 <AvidanB1704@gmail.com>
2023-04-26 11:43:19 +0200

    Création page Connexion

---
+++ src/component/Connexion.js
@@ -0,0 +1,35 @@
+import react from 'react';
+import { Link } from 'react-router-dom';
+import './Connexion.css';
+
+
+function Connexion() {
+    return (
+        <div>
+            <div className="connexion">
+                <h1>Qui est-ce ?</h1>
+            </div>
+            <div className="box">
+                <Link to ="/Homepage.js">
+                < div className="profil">
+                    <img src='src/component/cyrille-banovski.jpeg'alt="Cyrille Banovski"></img>
+                    <h2>Cyrille</h2>
+                </div>
+                </Link>
+                <Link to ="/Homepage.js">
+                < div className="profil">
+                    <img src='src/component/Brontis-Guilloux.jpeg'alt="Brontis Guilloux"></img>
+                    <h2>ErwinTheCat</h2>
+                </div>
+                </Link>
+                </div>
+            <div className="gestion">
+                <button>
+                    <h3>Gérer les profils</h3>
+                </button>
+            </div>
+        </div>
+    )
+}
+
+export default Connexion
