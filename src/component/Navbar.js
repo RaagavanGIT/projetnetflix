174256511c920cf5e514a7a2ab2cb57d29bc174e
Maxime Theophilos <maxtheo155@icloud.com>
2023-04-27 23:21:15 +0200

    Intégration de la searchbar fonctionnelle à la navbar

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -6,18 +6,37 @@ import Logo from "./image/Logo.svg";
 import Avatar from "./image/avatar.png";
 import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
 
-
 const Navbar = () => {
-  
-  const[isScrolled, setIsScrolled] = useState(false);
+  const [isScrolled, setIsScrolled] = useState(false);
+  const [isSearchOpen, setIsSearchOpen] = useState(false);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [searchResults, setSearchResults] = useState([]);
+
+  const handleSearchClick = () => {
+    setIsSearchOpen(!isSearchOpen);
+  };
+
+  const handleSearchChange = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const handleSearchSubmit = (event) => {
+    event.preventDefault();
+    const apiKey = "9e1d34262c59d085124aa1ee3b7065cb";
+    const apiUrl = `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&language=fr-FR&query=${searchTerm}`;
+
+    fetch(apiUrl)
+      .then((response) => response.json())
+      .then((data) => {
+        setSearchResults(data.results);
+      });
+  };
+
   window.onscroll = () => {
-    setIsScrolled(window.pageYOffset === 0 ? false : true)
+    setIsScrolled(window.pageYOffset === 0 ? false : true);
     return () => (window.onscroll = null);
+  };
 
-   
-  
-
-  }
   return (
     <div className={isScrolled ? "navbar scrolled" : "navbar"}>
       <div className="container">
@@ -31,11 +50,7 @@ const Navbar = () => {
           <span>Explorer par langue</span>
         </div>
         <div className="right">
-          <SearchIcon className="icon" > 
-
-
-          
-          </SearchIcon>
+          <SearchIcon className="icon" onClick={handleSearchClick} />
           <NotificationsIcon className="icon" />
           <img src={Avatar} />
           <div className="profile">
@@ -47,6 +62,29 @@ const Navbar = () => {
               <span>Centre d'aide</span>
             </div>
           </div>
+          {isSearchOpen && (
+            <form className="search-form" onSubmit={handleSearchSubmit}>
+              <input
+                type="text"
+                placeholder="Recherche"
+                value={searchTerm}
+                onChange={handleSearchChange}
+              />
+            </form>
+          )}
+          {searchResults.length > 0 && (
+            <div className="search-results">
+              {searchResults.map((result) => (
+                <div key={result.id} className="search-result">
+                  <img
+                    src={`https://image.tmdb.org/t/p/w92/${result.poster_path}`}
+                    alt={result.title}
+                  />
+                  <span>{result.title}</span>
+                </div>
+              ))}
+            </div>
+          )}
         </div>
       </div>
     </div>
